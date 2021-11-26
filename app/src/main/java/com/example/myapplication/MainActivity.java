fd9438ce4ce0e27f08d8c1790dc56f4b722c3a70
joellemeheshem12 <joellemeheshem12@gmail.com>
2021-11-26 10:02:05 +0200

    notification receiver

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +15,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int NOTIFICATION_REMINDER_NIGHT = 1;
     private Button buttonLogin,buttonSignUP;
     private Intent musicIntent;
 
@@ -25,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
         //this will start the service which in turn will the music
         musicIntent =new Intent(this,MusicService.class);
         startService(musicIntent);
+
+        Intent notifyIntent = new Intent(this,NotificationReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast
+                (this, NOTIFICATION_REMINDER_NIGHT, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,  System.currentTimeMillis(),
+                1000 * 60 * 60 * 24, pendingIntent);
     }
 
 
--- app/src/main/java/com/example/myapplication/NotificationReceiver.java
+++ app/src/main/java/com/example/myapplication/NotificationReceiver.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 
 public class NotificationReceiver extends BroadcastReceiver {
 
+    //this class is initiated when a broadcast is received from the OS
     @Override
     public void onReceive(Context context, Intent intent) {
-
+    Intent intent1 = new Intent(context,NotificationIntentService.class);
+    context.startService(intent1);
     }
 }
