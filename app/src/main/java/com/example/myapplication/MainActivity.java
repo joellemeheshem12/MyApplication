9760f70ef2170d78831847be06c39776be906b75
joellemeheshem12 <joellemeheshem12@gmail.com>
2022-02-21 22:06:08 +0200

    card view

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Submit2(View view) {
-        Intent intent= new Intent(this, MySwimmingTimesListActivity.class) ;
+        Intent intent= new Intent(this,MainActivity2.class) ;
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void onClick1(View view) {
+        Intent intent= new Intent(this,ArrayListActivity.class) ;
+        startActivity(intent);
+    }
+
+    public void onClick2(View view) {
+        Intent intent= new Intent(this,MySwimmingTimesListActivity.class) ;
+        startActivity(intent);
+    }
+
+    public void onClick3(View view) {
+        Intent intent= new Intent(this,MainActivity2.class) ;
+        startActivity(intent);
+    }
+}
